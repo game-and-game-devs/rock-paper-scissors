aa553554cbd217b6e2dc98ed04f8f4af0e51275d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-25 16:12:28 +0200

    Artículo finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { GameService } from './game.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'rock-paper-scissors';
+export class AppComponent implements OnInit {
+  result: string;
+  pointsUser = 0;
+  pointsComp =  0;
+
+  constructor(private playGame: GameService) {}
+
+  ngOnInit(): void {
+    this.result = 'Esperando jugada...';
+    console.log(this.pointsUser);
+  }
+
+  play(choice: string): void {
+    const result = this.playGame.game(choice);
+    console.log(result);
+    this.result = result.message;
+    this.pointsUser += result.userAdd;
+    this.pointsComp += result.compAdd;
+  }
 }
